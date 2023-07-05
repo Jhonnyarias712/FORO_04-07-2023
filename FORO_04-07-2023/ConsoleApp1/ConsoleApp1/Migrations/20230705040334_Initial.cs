96eb5d31a235d56f5582e4592ce2406bdb15e0f4
Jhonnyarias712 <88362486+Jhonnyarias712@users.noreply.github.com>
2023-07-04 23:19:27 -0500

    Actualizacion a 3 entidades propias :Auto,Propietario,Color
    +mainmenu
    +entrada de datos por consola
    +actualizacion del proyecto

---
+++ FORO_04-07-2023/ConsoleApp1/ConsoleApp1/Migrations/20230705040334_Initial.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConsoleApp1.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "colores",
+                columns: table => new
+                {
+                    Colorid = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_colores", x => x.Colorid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "propietarios",
+                columns: table => new
+                {
+                    Propietarioid = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Apellido = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Edad = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_propietarios", x => x.Propietarioid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "autos",
+                columns: table => new
+                {
+                    Autoid = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Modelo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Ano = table.Column<int>(type: "int", nullable: false),
+                    Propietarioid = table.Column<int>(type: "int", nullable: false),
+                    Colorid = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_autos", x => x.Autoid);
+                    table.ForeignKey(
+                        name: "FK_autos_colores_Colorid",
+                        column: x => x.Colorid,
+                        principalTable: "colores",
+                        principalColumn: "Colorid",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_autos_propietarios_Propietarioid",
+                        column: x => x.Propietarioid,
+                        principalTable: "propietarios",
+                        principalColumn: "Propietarioid",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_autos_Colorid",
+                table: "autos",
+                column: "Colorid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_autos_Propietarioid",
+                table: "autos",
+                column: "Propietarioid");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "autos");
+
+            migrationBuilder.DropTable(
+                name: "colores");
+
+            migrationBuilder.DropTable(
+                name: "propietarios");
+        }
+    }
+}
---
+++ FORO_04-07-2023/ConsoleApp1/ConsoleApp1/Models/SchoolContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Models
+{
+
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Auto> autos { get; set; }
+        public DbSet<Color> colores { get; set; }
+        public DbSet<Propietario> propietarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=DB_Auto_transaccion;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Auto>().HasKey(v => v.Autoid);
+            
+        }
+
+    }
+
+}
